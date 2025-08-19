0ae67177621ff6734095f795bbddee00e1222d78
Ganesh Lokhande <ganeshlokhande8068@gmail.com>
2025-08-19 18:46:04 +0530

    Firat Commit

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import ShopContextProvider from "./context/ShopContext";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
+);
